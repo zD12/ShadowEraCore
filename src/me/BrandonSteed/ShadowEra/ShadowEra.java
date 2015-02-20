84382114f7736995f82eb3091eb324ef03c32f76
GotNoFriends <piedoesmc1@gmail.com>
2015-02-20 14:39:09 +0000

    Edits with the PluginListener load

--- src/me/BrandonSteed/ShadowEra/ShadowEra.java
+++ src/me/BrandonSteed/ShadowEra/ShadowEra.java
@@ -1,33 +1,32 @@
-
 package me.BrandonSteed.ShadowEra;
 
+import me.BrandonSteed.ShadowEra.Commands.*;
 import net.minecraft.util.org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import me.BrandonSteed.ShadowEra.Commands.*;
 
 public class ShadowEra extends JavaPlugin {
-public ShadowEra plugin;
+ public PlayerListener PlayerListener;
+    public ShadowEra plugin;
      public void onEnable()
      {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(plugin.PlayerListener, plugin);
+         
         getCommand("rage").setExecutor((org.bukkit.command.CommandExecutor) new Command_rage(this));
         getCommand("announce").setExecutor((org.bukkit.command.CommandExecutor) new Command_announce(this));
         getCommand("administratemode").setExecutor((org.bukkit.command.CommandExecutor) new Command_administratemode(this));
-        getCommand("pl").setExecutor((org.bukkit.command.CommandExecutor) new Command_pl(this));
         getCommand("slime").setExecutor((org.bukkit.command.CommandExecutor) new Command_slime(this));
         getCommand("promote").setExecutor((org.bukkit.command.CommandExecutor) new Command_promote(this));
         getCommand("nv").setExecutor((org.bukkit.command.CommandExecutor) new Command_nv(this));
         getCommand("fakeleave").setExecutor((org.bukkit.command.CommandExecutor) new Command_fakeleave(this));
-        getCommand("serversay").setExecutor((org.bukkit.command.CommandExecutor) new Command_consolesay(this));
+        getCommand("consolesay").setExecutor((org.bukkit.command.CommandExecutor) new Command_consolesay(this));
         getCommand("machat").setExecutor((org.bukkit.command.CommandExecutor) new Command_machat(this));
-        getCommand("vj").setExecutor((org.bukkit.command.CommandExecutor) new Command_vj(this));
-        getCommand("csay").setExecutor((org.bukkit.command.CommandExecutor) new Command_csay(this));
-        
-        new PlayerListener(this);
         
         getLogger().info("[ShadowEraCore] Plugin by Got_No_Friends and VjGamingUK2000.");
         Bukkit.broadcastMessage(ChatColor.BLUE + "[ShadowEraCore] " + ChatColor.DARK_PURPLE + "Plugin by Got_No_Friends and VjGamingUK2000 sucessfully enabled!");
