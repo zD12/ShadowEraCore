586004cc65b30b2077fc2830fd3367c049e67cb0
GotNoFriends <piedoesmc1@gmail.com>
2015-02-20 15:38:25 +0000

    Edits so it won't break the commands >.>

--- src/me/BrandonSteed/ShadowEra/ShadowEra.java
+++ src/me/BrandonSteed/ShadowEra/ShadowEra.java
@@ -11,14 +11,20 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ShadowEra extends JavaPlugin {
- public PlayerListener PlayerListener;
+ public PlayerListener playerListener;
     public ShadowEra plugin;
-     public void onEnable()
+     
+    @Override
+    public void onLoad() {
+        plugin = this;
+        playerListener = new PlayerListener(plugin);
+    }
+    
+    public void onEnable()
      {
         PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(plugin.PlayerListener, plugin);
-         
-        getCommand("rage").setExecutor((org.bukkit.command.CommandExecutor) new Command_rage(this));
+        pm.registerEvents(plugin.playerListener, plugin);
+         getCommand("rage").setExecutor((org.bukkit.command.CommandExecutor) new Command_rage(this));
         getCommand("announce").setExecutor((org.bukkit.command.CommandExecutor) new Command_announce(this));
         getCommand("administratemode").setExecutor((org.bukkit.command.CommandExecutor) new Command_administratemode(this));
         getCommand("slime").setExecutor((org.bukkit.command.CommandExecutor) new Command_slime(this));
@@ -27,7 +33,7 @@ public class ShadowEra extends JavaPlugin {
         getCommand("fakeleave").setExecutor((org.bukkit.command.CommandExecutor) new Command_fakeleave(this));
         getCommand("consolesay").setExecutor((org.bukkit.command.CommandExecutor) new Command_consolesay(this));
         getCommand("machat").setExecutor((org.bukkit.command.CommandExecutor) new Command_machat(this));
-        
+
         getLogger().info("[ShadowEraCore] Plugin by Got_No_Friends and VjGamingUK2000.");
         Bukkit.broadcastMessage(ChatColor.BLUE + "[ShadowEraCore] " + ChatColor.DARK_PURPLE + "Plugin by Got_No_Friends and VjGamingUK2000 sucessfully enabled!");
        
