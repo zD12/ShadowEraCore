71352a920c2de5bdf6c9bba43f9a23d2bf26b51e
VjGamingUK <vj13573@hotmail.com>
2015-02-23 21:52:52 +0000

    Unfinished; Will finish this off tomorrow...

--- src/me/BrandonSteed/ShadowEra/Commands/Command_fuck.java
+++ src/me/BrandonSteed/ShadowEra/Commands/Command_fuck.java
@@ -1,1 +1,33 @@
+// Unfished, finishing this off tomorrow, if you wanna edit it remove the "/*".
+// The plan is here: https://github.com/GotNoFriends/ShadowEraMod/blob/master/src/me/StevenLawson/TotalFreedomMod/Commands/Command_fuck.java
+// ...Just editing the permissions and bits of stuff :P
+// ...since that one is using TFM perms.
 
+/*
+
+package me.BrandonSteed.ShadowEra.Commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Command_fuck implements CommandExecutor;
+{
+  public ShadowEra plugin;
+  
+  public Command_fuck(ShadowEra plugin)
+  {
+    this.plugin = plugin;
+  }
+  
+  @Override
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+  {
+    int length = args.length;
+
+    return true;
+  }
+}
