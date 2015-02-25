c9977b45ec604ab6d8c46f29f7b55fd7ee4ca13c
VjGamingUK <vj13573@hotmail.com>
2015-02-25 17:33:22 +0000

    Update Command_forcecommand.java

--- src/me/BrandonSteed/ShadowEra/Commands/Command_forcecommand.java
+++ src/me/BrandonSteed/ShadowEra/Commands/Command_forcecommand.java
@@ -1,1 +1,67 @@
+package me.BrandonSteed.ShadowEra.Commands;
 
+import java.util.Arrays;
+import me.BrandonSteed.ShadowEra.ShadowEra;
+import net.minecraft.util.org.apache.commons.lang3.ArrayUtils;
+import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.bukkit.Bukkit;
+import static org.bukkit.Bukkit.getServer;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Command_forcecommand implements CommandExecutor;
+{
+    public ShadowEra plugin; 
+
+    public Command_machat(ShadowEra plugin) 
+    {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args)
+    {
+      int length = args.length;
+      
+      if (length == 0)
+      {
+        sender.sendMessage(ChatColor.WHITE + "/forcecommand <player> <command> [args.]");
+        return true;
+      }
+      
+      if (length == 1)
+      {
+        boolean playerFound = false;
+        
+        for (Player player : Bukkit.getServer().getOnlinePlayers())
+        {
+          if (args[0].equals(sender.getName())
+          {
+            sender.sendMessage(ChatColor.RED + "Dumbass.");
+            return true;
+          }
+          
+          if (player.getName().equalsIgnoreCase(args[0]))
+          {
+            player.performCommand((StringUtils.join(args, " ")));
+            playerFound = true;
+            break;
+          }
+        }
+        
+        if (playerFound == false)
+        {
+          sender.sendMessage(ChatColor.DARK_RED + "Error: " + ChatColor.RED + args[0] + " was not found!");
+          return true;
+        }
+        
+        return true;
+      }
+      
+      return true;
+    }
+}
