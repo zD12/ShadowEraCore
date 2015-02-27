9d9549ef1a3c30f32ce134f36ea736d253cfdcde
GotNoFriends <piedoesmc1@gmail.com>
2015-02-27 18:32:08 +0000

    For the new playerlistener location.

--- src/me/BrandonSteed/ShadowEra/ShadowEra.java
+++ src/me/BrandonSteed/ShadowEra/ShadowEra.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import me.BrandonSteed.ShadowEra.Listeners.PlayerListener;
 
 public class ShadowEra extends JavaPlugin {
  public PlayerListener playerListener;
