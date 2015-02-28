10610ab849c0c63a77578c3ea80e029711cd4540
GotNoFriends <piedoesmc1@gmail.com>
2015-02-28 11:30:41 +0000

    You can now run commands as console, added perms and tidy up

--- src/me/BrandonSteed/ShadowEra/Commands/Command_console.java
+++ src/me/BrandonSteed/ShadowEra/Commands/Command_console.java
@@ -21,17 +21,50 @@ public class Command_console implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+         player Player = (Player) sender;
+         if (! (player.hasPermission("shadoweracore.command.console"))) {
+          sender.sendMessage(ChatColor.DARK_RED + "You do not have access to that command.");
+          return true;
+         }
+         
          if (args.length == 0)
             {
                 sender.sendMessage(ChatColor.DARK_RED + "null.");
             }
             
-            if (args.length > 0)
+         if (args[0].equalsIgnoreCase("say"))
             {
+                if (args.length == 1)
+                {
+                    player.sendMessage(ChatColor.RED + "Usage: /console say <message>");
+                    event.setCancelled(true);
+                    return;
+                }
+                if (args.length > 0)
+                {
                 Bukkit.broadcastMessage(String.format("§7[Console: " + sender.getName() + " §c ", StringUtils.join(args, " ")));
-            }
+                }
+            }    
+
             
+        else if (args[0].equalsIgnoreCase("runcmd"))
+            {
+                if (args.length == 1)
+                {
+                    player.sendMessage(ChatColor.RED + "Usage: /console runcmd <command>");
+                    event.setCancelled(true);
+                    return;
+                }
+                String command = "";
+                for (i = 1; i < args.length; i++)
+                {
+                    command = command + args[i] + " ";
+                }
+                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), command.trim());
+                event.setCancelled(true);
+                return;
+            }    
         return false;
-       
+    }
     }      
 }
