9fdcb4739ceaa2cac18eb9764d50983d82916ae5
GotNoFriends <piedoesmc1@gmail.com>
2015-02-28 22:21:05 +0000

    Added the announcement tag and edits

--- src/me/zd12/ShadowEra/SE_Messages.java
+++ src/me/zd12/ShadowEra/SE_Messages.java
@@ -4,7 +4,7 @@ import org.bukkit.ChatColor;
 // Work in progress, likely will take a while until fully configured.
 // Quick access to modify *most* messages. 
 public class SE_Messages {
-   public static final String NO_PLAYER = "That username cannot be found. Ensure your spelling is correct.";
+   public static final String NO_PLAYER = ChatColor.RED + "That username cannot be found. Ensure your spelling is correct.";
    public static final String NO_PREMS = ChatColor.DARK_RED + "You don't have the required permissions to execute this command.";
    public static final String USAGE = ChatColor.GRAY + "The proper usage for this command is: ";
    public static final String SWEAR = ChatColor.GRAY + "Swearing is forbidden!";
@@ -13,4 +13,6 @@ public class SE_Messages {
    public static final String TAG = ChatColor.WHITE + "[" + ChatColor.AQUA + "Shadow" + ChatColor.GREEN + "era" + ChatColor.LIGHT_PURPLE + "Core" + ChatColor.WHITE + "] " + ChatColor.RESET;
    public static final String TAG_MOD = ChatColor.WHITE + "[" + ChatColor.AQUA + "Shadow" + ChatColor.GREEN + "era" + ChatColor.LIGHT_PURPLE + "Mod" + ChatColor.WHITE + "] " + ChatColor.RESET;
    public static final String NOTCH = "<" + ChatColor.GOLD + "Notch" + ChatColor.WHITE + "> "; 
+   public static final String ANNOUNCEMENT_TAG = ChatColor.RED + ChatColor.BOLD + "ShadowEra " + ChatColor.GREEN + ChatColor.BOLD + "> ";
+   
 }
