60766c6826755547e2bade3fe40c17189b02d6fc
GotNoFriends <piedoesmc1@gmail.com>
2015-03-02 17:02:01 +0000

    Added my listener poop

--- src/me/BrandonSteed/ShadowEra/ShadowEra.java
+++ src/me/BrandonSteed/ShadowEra/ShadowEra.java
@@ -1,6 +1,8 @@
 package me.BrandonSteed.ShadowEra;
 
 import me.BrandonSteed.ShadowEra.Commands.Command_shadoweracore;
+import me.BrandonSteed.ShadowEra.Listeners.PlayerListener;
+import me.BrandonSteed.ShadowEra.Listeners.ThorListener;
 import net.pravian.bukkitlib.BukkitLib;
 import net.pravian.bukkitlib.command.BukkitCommandHandler;
 import net.pravian.bukkitlib.config.YamlConfig;
@@ -8,12 +10,15 @@ import net.pravian.bukkitlib.implementation.BukkitPlugin;
 import net.pravian.bukkitlib.util.LoggerUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.PluginManager;
 
 public class ShadowEra extends BukkitPlugin
 {
 
     public static ShadowEra plugin;
     public BukkitCommandHandler handler;
+    public PlayerListener playerListener;
+    public ThorListener thorHammer;
 
 
     // YAML File Information
@@ -25,11 +30,18 @@ public class ShadowEra extends BukkitPlugin
     {
      this.plugin = this;
      this.handler = new BukkitCommandHandler(plugin); // Initialize the command handler
+     playerListener = new PlayerListener(plugin); // PlayerListener thingy
+     thorHammer = new ThorListener(plugin);
     }
 
     @Override
     public void onEnable()
     {
+        // Listener Crap
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(plugin.playerListener, plugin);
+        pm.registerEvents(plugin.thorHammer, plugin);
+
         // Bukkit Lib Important Stuff
         BukkitLib.init(plugin);
         handler.setCommandLocation(Command_shadoweracore.class.getPackage());
