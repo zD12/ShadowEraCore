ce398b816616b1164a710c8a1f4f40c16f918d49
GotNoFriends <piedoesmc1@gmail.com>
2015-03-02 16:47:43 +0000

    Update Command_fakeleave.java

--- src/me/BrandonSteed/ShadowEra/Commands/Command_fakeleave.java
+++ src/me/BrandonSteed/ShadowEra/Commands/Command_fakeleave.java
@@ -1,25 +1,20 @@
 package me.BrandonSteed.ShadowEra.Commands;
 
-import me.BrandonSteed.ShadowEra.ShadowEra;
+import me.BrandonSteed.ShadowEra.SE_Messages;
+import net.pravian.bukkitlib.command.BukkitCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Command_fakeleave implements CommandExecutor {
+public class Command_fakeleave extends BukkitCommand {
 
-    public ShadowEra plugin; // points to the main class
-    
-    public Command_fakeleave(ShadowEra plugin) {
-        this.plugin = plugin;
-    }
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean run(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;
         if (! (player.hasPermission("shadoweracore.command.fakeleave"))) {
-          sender.sendMessage(ChatColor.DARK_RED + "You do not have access to that command.");
+          sender.sendMessage(SE_Messages.NO_PREMS);
           return true;
           }
           Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.GREEN + "-" + ChatColor.GRAY + "] " + player.getName());
