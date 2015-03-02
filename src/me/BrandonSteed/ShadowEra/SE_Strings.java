c0b3965b4aaa390fe07df780634b6b987cf80c1f
GotNoFriends <piedoesmc1@gmail.com>
2015-03-02 16:32:56 +0000

    Update and rename src/me/BrandonSteed/ShadowEra to src/me/BrandonSteed/ShadowEra/SE_Strings.java

---
+++ src/me/BrandonSteed/ShadowEra/SE_Strings.java
@@ -0,0 +1,12 @@
+package me.BrandonSteed.ShadowEra;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+// Work in progress, likely will take a while until fully configured.
+// Quick access to modify *most* strings. 
+
+public class SE_Strings {
+   public static final Player OWNER = Bukkit.getServer().getPlayer("Got_No_Friends");
+}
