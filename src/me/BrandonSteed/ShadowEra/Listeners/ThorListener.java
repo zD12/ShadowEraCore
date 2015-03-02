cd47ce580f8ee82eaa76f6dd604b6f8111a24a42
GotNoFriends <piedoesmc1@gmail.com>
2015-03-02 16:36:50 +0000

    ThorHammer.

---
+++ src/me/BrandonSteed/ShadowEra/Listeners/ThorListener.java
@@ -0,0 +1,54 @@
+package me.BrandonSteed.ShadowEra.Listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class ThorListener implements Listener {
+          
+  @EventHandler(priority = EventPriority.HIGH)
+    public void onPlayerInteract(PlayerInteractEvent event)
+    {
+        final Player player = event.getPlayer();
+        
+        switch (event.getAction())
+        {
+            case RIGHT_CLICK_AIR:
+            case RIGHT_CLICK_BLOCK:
+            {
+                switch (event.getMaterial())
+                {
+                    case GOLD_AXE:
+                    {
+                        if (player.hasPermission("shadoweracore.event.thor"))
+                        {
+                            for (int i = 0; i <= 100; i++)
+                            {
+                                player.getWorld().strikeLightning(player.getTargetBlock(null, 500).getLocation());
+                            }
+                            player.getWorld().createExplosion(player.getTargetBlock(null, 500).getLocation(), 4F);
+                        }
+                        else
+                        {
+                            event.setCancelled(true);
+                        }
+         
+                        break;
+                    }
+                }
+            }
+            
+            case LEFT_CLICK_AIR:
+            case LEFT_CLICK_BLOCK:
+            {
+                switch (event.getMaterial())
+                {
+                    // none yet
+                }  
+       
+            }
+}
+    }
+}
