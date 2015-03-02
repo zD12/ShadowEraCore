89e7381ff049c786264c955a5e903973107bc47c
GotNoFriends <piedoesmc1@gmail.com>
2015-03-02 16:55:01 +0000

    Update Command_rage.java

--- src/me/BrandonSteed/ShadowEra/Commands/Command_rage.java
+++ src/me/BrandonSteed/ShadowEra/Commands/Command_rage.java
@@ -1,24 +1,17 @@
 package me.BrandonSteed.ShadowEra.Commands;
 
-import me.BrandonSteed.ShadowEra.ShadowEra;
-import me.zd12.ShadowEra.SE_Messages;
+import me.BrandonSteed.ShadowEra.SE_Messages;
+import net.pravian.bukkitlib.command.BukkitCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Command_rage implements CommandExecutor {
-
-    public ShadowEra plugin; // points to the main class
-
-    public Command_rage(ShadowEra plugin) {
-        this.plugin = plugin;
-    }
+public class Command_rage extends BukkitCommand {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean run(CommandSender sender, Command cmd, String label, String[] args) {
           Player player = (Player) sender;
           if (! (player.hasPermission("shadoweracore.command.rage"))) {
           sender.sendMessage(SE_Messages.NO_PREMS);
