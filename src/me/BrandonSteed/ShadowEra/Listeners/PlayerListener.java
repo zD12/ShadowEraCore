2e6d848f2b2669850910c2802ace125fe6346d6e
GotNoFriends <piedoesmc1@gmail.com>
2015-03-02 16:35:25 +0000

    Updated, moving the thorhammer to a different place.

--- src/me/BrandonSteed/ShadowEra/Listeners/PlayerListener.java
+++ src/me/BrandonSteed/ShadowEra/Listeners/PlayerListener.java
@@ -1,8 +1,7 @@
 package me.BrandonSteed.ShadowEra.Listeners;
  
 import me.BrandonSteed.ShadowEra.ShadowEra;
-import me.zd12.ShadowEra.SE_Messages;
-import org.bukkit.ChatColor;
+import me.BrandonSteed.ShadowEra.SE_Messages;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -21,66 +20,13 @@ public class PlayerListener implements Listener
      
  @EventHandler
     public void onPlayerChat(PlayerChatEvent event) {
-        /*if(event.getMessage().equalsIgnoreCase("/pl") || event.getMessage().equalsIgnoreCase("/plugins") || event.getMessage().equalsIgnoreCase("/bukkit:plugins") || event.getMessage().equalsIgnoreCase("/bukkit:pl"))
-        event.getPlayer().sendMessage(ChatColor.GREEN + "This command is blocked!!");
-        event.setCancelled(true); */  //Enabled '/plugins'
         if(event.getMessage().contains("fuck") || event.getMessage().equalsIgnoreCase("bitch") || event.getMessage().contains("nigg"))
         event.getPlayer().sendMessage(SE_Messages.SWEAR);
         event.setCancelled(true);
+       
         if(event.getMessage().contains("cunt") || event.getMessage().contains("porn") || event.getMessage().contains("horny") || event.getMessage().contains("dick") || event.getMessage().contains("penis") || event.getMessage().contains("cock"))
         event.getPlayer().sendMessage(SE_Messages.CHILDREN_FRIENDLY_PLEASE);
         event.setCancelled(true);
     }
-        
-/*        @EventHandler(priority = EventPriority.HIGHEST)
-        public void onPlayerJoin(PlayerJoinEvent e)
-        {
-                e.getPlayer().sendMessage(ChatColor.RED + "This server is using ShadowEraCore!");
-                e.getPlayer().sendMessage(ChatColor.RED + "To get the source go to github.com/GotNoFriends/ShadowEraCore");
-        } */  // We don't wanna advertise every login- it's annoying and spammy.  
-      
-  @EventHandler(priority = EventPriority.HIGH)
-    public void onPlayerInteract(PlayerInteractEvent event)
-    {
-        final Player player = event.getPlayer();
-        
-        switch (event.getAction())
-        {
-            case RIGHT_CLICK_AIR:
-            case RIGHT_CLICK_BLOCK:
-            {
-                switch (event.getMaterial())
-                {
-                    case GOLD_AXE:
-                    {
-                        if (player.hasPermission("shadoweracore.event.thor"))
-                        {
-                            for (int i = 0; i <= 100; i++)
-                            {
-                                player.getWorld().strikeLightning(player.getTargetBlock(null, 500).getLocation());
-                            }
-                            player.getWorld().createExplosion(player.getTargetBlock(null, 500).getLocation(), 4F);
-                        }
-                        else
-                        {
-                            event.setCancelled(true);
-                        }
-         
-                        break;
-                    }
-                }
-            }
-            
-            case LEFT_CLICK_AIR:
-            case LEFT_CLICK_BLOCK:
-            {
-                switch (event.getMaterial())
-                {
-                    // none yet
-                }  
-       
-            }
-        
-        }
-    }
+  
 }
