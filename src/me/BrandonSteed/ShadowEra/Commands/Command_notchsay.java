b92fe7cc868facf9879ce71980adde345ecfe495
GotNoFriends <piedoesmc1@gmail.com>
2015-03-02 16:54:01 +0000

    Update and rename src/me/BrandonSteed/ShadowEra/Commands to src/me/BrandonSteed/ShadowEra/Commands/Command_notchsay.java

---
+++ src/me/BrandonSteed/ShadowEra/Commands/Command_notchsay.java
@@ -0,0 +1,27 @@
+package me.BrandonSteed.ShadowEra.Commands;
+
+import me.BrandonSteed.ShadowEra.SE_Messages;
+import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+import net.pravian.bukkitlib.command.BukkitCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Command_notchsay extends BukkitCommand {
+
+
+    @Override
+    public boolean run(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player = (Player) sender;    
+        if (! (player.hasPermission("shadoweracore.command.notchsay"))) {
+          sender.sendMessage(SE_Messages.NO_PREMS);
+          return true;
+       }
+        String message = StringUtils.join(args, " ");
+        
+        Bukkit.broadcastMessage(SE_Messages.NOTCH + message);
+        return false;
+
+    }
+}
