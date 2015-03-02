dd4ed89680da235ba8d268a6f305ec804df2f855
GotNoFriends <piedoesmc1@gmail.com>
2015-03-02 16:47:14 +0000

    Update Command_consolesay.java

--- src/me/BrandonSteed/ShadowEra/Commands/Command_consolesay.java
+++ src/me/BrandonSteed/ShadowEra/Commands/Command_consolesay.java
@@ -1,8 +1,9 @@
 package me.BrandonSteed.ShadowEra.Commands;
 
+import me.BrandonSteed.ShadowEra.SE_Messages;
 import me.BrandonSteed.ShadowEra.ShadowEra;
-import me.zd12.ShadowEra.SE_Messages;
 import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+import net.pravian.bukkitlib.command.BukkitCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -10,16 +11,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Command_consolesay implements CommandExecutor {
-
-    public ShadowEra plugin; // points to the main class
-
-    public Command_consolesay(ShadowEra plugin) {
-        this.plugin = plugin;
-    }
+public class Command_consolesay extends BukkitCommand {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean run(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;
         if (! (player.hasPermission("shadoweracore.command.consolesay"))) {
           sender.sendMessage(SE_Messages.NO_PREMS);
