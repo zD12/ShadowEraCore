3a5b4badbaf4ee5c1ba84fce0082fe1c9a8eadaa
GotNoFriends <piedoesmc1@gmail.com>
2015-03-02 16:44:08 +0000

    BukkitLib.

--- src/me/BrandonSteed/ShadowEra/Commands/Command_announce.java
+++ src/me/BrandonSteed/ShadowEra/Commands/Command_announce.java
@@ -1,25 +1,17 @@
 package me.BrandonSteed.ShadowEra.Commands;
 
-import me.BrandonSteed.ShadowEra.ShadowEra;
-import me.zd12.ShadowEra.SE_Messages;
+import me.BrandonSteed.ShadowEra.SE_Messages;
 import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+import net.pravian.bukkitlib.command.BukkitCommand;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Command_announce implements CommandExecutor {
-
-    public ShadowEra plugin; // points to the main class
-
-    public Command_announce(ShadowEra plugin) {
-        this.plugin = plugin;
-    }
+public class Command_announce extends BukkitCommand {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean run(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;    
         if (! (player.hasPermission("shadoweracore.command.announce"))) {
           sender.sendMessage(SE_Messages.NO_PREMS);
